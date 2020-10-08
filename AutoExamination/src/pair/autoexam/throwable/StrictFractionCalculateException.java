fb4f03e4492ec383ed9ab28e0c53096083a5dbb7
mortal-github <13710503031@139.com>
2020-10-08 13:48:10 +0800

    add class StrictFraction to calculate strict fraction(only positive number)

---
+++ AutoExamination/src/pair/autoexam/throwable/StrictFractionCalculateException.java
@@ -0,0 +1,19 @@
+package pair.autoexam.throwable;
+
+public class StrictFractionCalculateException extends Exception{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1254417472603297624L;
+
+	public StrictFractionCalculateException()
+	{
+		super();
+	}
+
+	public StrictFractionCalculateException(String message)
+	{
+		super(message);
+	}
+}
